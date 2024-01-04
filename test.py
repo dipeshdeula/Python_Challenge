a181bedf111652cfc26d224c59990604e0510ea5
dipeshdeula <deuladipesh94@gmail.com>
2024-01-04 19:50:55 +0545

    basic test completed

---
+++ test.py
@@ -0,0 +1,22 @@
+# age=23
+# if(age>18):
+#     print("you adult");
+# else:
+#     print("you child")
+# names=['john','mike']
+# print(len(names))
+
+# def add(num1,num2):
+#     return num1+num2
+# add(3,8)
+
+# data=[5,6,8,9,1,3,4]
+# print(data[::-2])
+# for i in range(2,10):
+#     if i%2==0:
+#         print(i)
+
+# for i in range(2):
+#     print(i)
+
+print("hello\nworld")
