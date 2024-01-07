9003d941703a7be9d4fb74b513b6662a7f575f14
dipeshdeula <deuladipesh94@gmail.com>
2024-01-07 14:56:10 +0545

    Day 10 practice session updated

--- Day10.py
+++ Day10.py
@@ -41,3 +41,54 @@
 # print(" ")
 
 #convert a binary number to decimal number using a recursive function
+
+
+#simple intrest problem
+
+#you borrowed $5000 for 2 years with 2% interest per year
+#Calculate the simple interest to know how much you have to pay?
+# def calc_interest():
+#     p = int(input('Enter Principal amount:'))
+#     t = int(input('Enter Time period:'))
+#     r = float(input('Enter Rate of Interest:(in percentage)'))/100
+
+#     #formula to calculate simple interest
+#     SI = (p*t*r)/100
+#     print(f'The Simple Interest is :${SI:.2f}')
+# calc_interest()
+
+#calculate compound interest
+# def cal_comopound_interest():
+#     P = float(input('Enter the principal amount:'))
+#     T = float(input('Enter the time period:'))
+#     R = float(input('Enter the rate of interest:(in percentage)'))/100
+#     N = int(input('Enter the number of times interest is paid per year:'))
+#     #Formula to calculate compound interest
+#     A=P * pow((1+ (R/N)),N*T)
+#     CI=(A-P)
+#     print(f'The Compound Interest is ${CI:.2f}')
+# cal_comopound_interest()
+
+#Calculate grade of five subjects
+def calculateGrade():
+    print('Enter your marks of each subject')
+    sub1=int(input("first subject:"))
+    sub2=int(input("second subject:"))
+    sub3=int(input("Third subject:"))
+    sub4=int(input("fourth subject:"))
+    sub5=int(input("fifth subject:"))
+
+    avg=(sub1+sub2+sub3+sub4+sub5)/5
+
+    if avg>=90:
+        print("Your Grade is A")
+    elif avg>=80:
+        print("Your Grade is B")
+    elif avg>=70:
+        print("Your Grade is C")
+    elif avg>=60:
+        print("Your Grade is D")
+    else:
+        print("Your Grade is F")
+calculateGrade()
+       
